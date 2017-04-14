5b0458d1b408630034b2e74e6ab40fddef5adca9
christopherfracassi <fracassc@mail.gvsu.edu>
2017-04-13 22:03:28 -0400

    dart animation

--- app/js/main.js
+++ app/js/main.js
@@ -7,6 +7,7 @@ let dartBox;//, cubeBox1, cubeBox2;
 let scoreChangeNeeded;
 let mouse, raycaster;
 let score;
+let shootDart;
 let gui;
 let objects = [];
 let popableBalloons = [];
@@ -29,6 +30,7 @@ function init() {
     dartMat = new THREE.MeshPhongMaterial({color: 0xffffff});
     dart = new THREE.Mesh(dartGeo, dartMat);
     dart.rotateX(-20);
+    dart.position.set(0,0,100);
     scene.add(dart);
     dartBox = new THREE.Box3().setFromObject(dart);
 
@@ -45,6 +47,27 @@ function init() {
         scene.add( object );
     });
 
+
+    /* Balloon Stand Model */
+
+    loader.load("models/stand.json",function ( object ) {
+        object.scale.set( 6000, 6000, 6000 );
+        object.translateX(-1000);
+        object.translateY(-400);
+        object.translateZ(-1500);
+        object.rotateY(-3.5);
+        scene.add( object );
+    });
+
+    loader.load("models/stand.json",function ( object ) {
+        object.scale.set( 6000, 6000, 6000 );
+        object.translateX(1000);
+        object.translateY(-400);
+        object.translateZ(-1500);
+        object.rotateY(.5);
+        scene.add( object );
+    });
+
     for (let i = 0; i < 7; i++) {
         let cubeGeo = new THREE.BoxGeometry(50, 50, 50);
         let cubeMat = new THREE.MeshPhongMaterial({color: 0xff0000});
@@ -58,25 +81,12 @@ function init() {
         cubeBoxes.push(cubeBox);
     }
 
-    // cubeGeo1 = new THREE.BoxGeometry(50, 50, 50);
-    // cubeMat1 = new THREE.MeshPhongMaterial({color: 0xff0000});
-    // goodCube1 = new THREE.Mesh(cubeGeo1, cubeMat1);
-    // goodCube1.translateX(Math.floor(Math.random() * (270 - (-270))) + (-270));
-    // goodCube1.translateY(Math.floor(Math.random() * (150 - (-150))) + (-150));
-    // scene.add(goodCube1);
-    // objects.push(goodCube1);
-    // popableBalloons.push(goodCube1);
-    // cubeBox1 = new THREE.Box3().setFromObject(goodCube1);
-    //
-    // cubeGeo2 = new THREE.BoxGeometry(50, 50, 50);
-    // cubeMat2 = new THREE.MeshPhongMaterial({color: 0xff0000});
-    // goodCube2 = new THREE.Mesh(cubeGeo2, cubeMat2);
-    // goodCube2.translateX(Math.floor(Math.random() * (270 - (-270))) + (-270));
-    // goodCube2.translateY(Math.floor(Math.random() * (150 - (-150))) + (-150));
-    // scene.add(goodCube2);
-    // objects.push(goodCube2);
-    // popableBalloons.push(goodCube2);
-    // cubeBox2 = new THREE.Box3().setFromObject(goodCube1);
+    cubeGeo = new THREE.BoxGeometry(1000000, 1, 100000);
+    cubeMat = new THREE.MeshLambertMaterial({color: 0xA0522D});
+    let ground = new THREE.Mesh(cubeGeo, cubeMat);
+    ground.translateY(-402);
+    scene.add(ground);
+    objects.push(ground);
 
     const lightOne = new THREE.DirectionalLight(0xFFFFFF, 1.0);
     lightOne.position.set(10, 40, 200);
@@ -117,22 +127,22 @@ function init() {
             controls.enableRotate ? controls.enableRotate = false : controls.enableRotate = true;
         }
         if (key.keyCode == 87) { //w
-            dart.translateY(20);
+            dart.position.y += 20;
         }
         if (key.keyCode == 65) { //a
-            dart.translateX(-20);
+            dart.position.x -= 20;
         }
         if (key.keyCode == 83) { //s
-            dart.translateY(-20);
+            dart.position.y -=20;
         }
         if (key.keyCode == 68) { //d
-            dart.translateX(20);
+            dart.position.x += 20;
         }
         if (key.keyCode == 82) { //r
-            dart.translateZ(20);
+            dart.position.z +=20;
         }
         if (key.keyCode == 70) { //f
-            dart.translateZ(-20);
+            dart.position.z  -=20;
         }
 
     };
@@ -145,97 +155,19 @@ function init() {
     document.addEventListener( 'mousedown', onDocumentMouseDown, false );
     document.addEventListener( 'touchstart', onDocumentTouchStart, false );
 
-
 }
-
-/**
- * @param event                 The event.
- * @param event.touches         The event's touches array
- * @param event.preventDefault  prevents the default event
- * @param event.clientX         x
- * @param event.clientY         y
- */
 function onDocumentTouchStart( event ) {
 
     event.preventDefault();
 
-    event.clientX = event.touches[0].clientX;
-    event.clientY = event.touches[0].clientY;
     onDocumentMouseDown( event );
 
 }
 
 function onDocumentMouseDown( event ) {
 
-    event.preventDefault();
-
-    mouse.x = ( event.clientX / renderer.domElement.clientWidth ) * 2 - 1;
-    mouse.y = -( event.clientY / renderer.domElement.clientHeight ) * 2 + 1;
-
-    raycaster.setFromCamera(mouse, camera);
-
-    let intersects = raycaster.intersectObjects(objects);
-
-    if (intersects.length > 0) {
-        if(intersects[0].object == mesh) {
-            intersects[0].object.material.color.setHex(Math.random() * 0xffffff);
-            score--;
-            document.getElementById("score").textContent="Score :" + score;
-        }
-        if(popableBalloons.includes(intersects[0].object)){
-            intersects[0].object.material.color.setHex(Math.random() * 0xffffff);
-            score++;
-            document.getElementById("score").textContent="Score :" + score;
-            Console.log("Score: " + score);
-        }
-
-        if (score == 10){
-
-            score = 0;
-
-            for (let mesh in popableBalloons) {
-                let balloon = popableBalloons[mesh];
-                scene.remove(balloon);
-            }
-            scene.remove(mesh);
-
-            let loader = new THREE.FontLoader();
-            loader.load( 'fonts/Immortal_Regular.json', function ( font ) {
-
-                let textGeometry = new THREE.TextGeometry( "You Won!", {
-
-                    font: font,
-
-                    size: 100,
-                    height: 30,
-                    curveSegments: 20,
-
-                    bevelThickness: 3,
-                    bevelSize: 3,
-                    bevelEnabled: true
-
-                });
-
-                let textMaterial = new THREE.MeshPhongMaterial(
-                    { color: 0xff0000, specular: 0xffffff }
-                );
-
-                let text = new THREE.Mesh( textGeometry, textMaterial );
-                text.translateX(-370);
-                scene.add( text );
-
-            });
-
-           // winner();
-        }
-
-        //TODO: Potential code to remove object after clicked
-        //scene.remove(intersects[0].object);
-        //"move" to trash so that that location won't trigger a collision after object removed
-       //let movedObj = intersects[0].object;
-        //movedObj.position.set(99999,9999,9999);
-
-    }
+    shootDart = true;
+    update();
 
 }
 
@@ -249,8 +181,23 @@ function update(){
         if (dartBox.intersectsBox(cubeBoxes[i])) {
             removeObject(popableBalloons[i]);
             updateScore(true);
+            resetDart();
         }
     }
+
+    if(dart.position.z < -20){
+        resetDart();
+    }
+
+    if(shootDart){
+        dart.position.z += -2;
+        dart.rotateY(0.1);
+    }
+}
+
+function resetDart(){
+    dart.position.set(0,0,100);
+    shootDart = false;
 }
 
 function removeObject(object) {
@@ -268,7 +215,7 @@ function updateScore(point) {
     scoreChangeNeeded = false;
 
     document.getElementById("score").textContent="Score :" + score;
-    if (score == 2) {
+    if (score == 7) {
 
         score = 0;
 
